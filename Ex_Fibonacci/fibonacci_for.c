d79f312472ed5ceec1053b0b4d5f226250108ffc
Gukanaiama <gukanaiama@usp.br>
2022-09-22 13:56:45 -0300

    Correção de bug das seq fibonacci, antes não pareavam.

--- Ex_Fibonacci/fibonacci_for.c
+++ Ex_Fibonacci/fibonacci_for.c
@@ -2,10 +2,17 @@
 
 int fib_recursivo(int n){
 
+    //Caso base quando n vale 0
+    if(n == 0){
+        return 0;
+    }
+
+    //Caso base quando n vale 1
     if(n == 1){
         return 0;
     }
 
+    //Caso base quando n vale 2
     if(n == 2){
         return 1;
     }
@@ -14,24 +21,36 @@ int fib_recursivo(int n){
 }
 
 
-int main(int n) {
+int main(void) {
 
+    int n;
+
+    //Input para o índice do Fibonacci
+    scanf("%d", &n);
+
+    //Inicializa 'num' em 0
     int num = 0;
-    int ant = 1;
+    int ant = 0;
     int ult = 1;
 
-    for(int i=0; i < n; i++){
+
+    //Caso excepcional
+    if(n == 2){
+        num = 1;
+    }
+
+
+    for(int i=2; i < n; i++){
 
         num = ant + ult;
-
 
         ant = ult;
         ult = num;
 
-
     }
     
-    printf("%d\n", num);
+    printf("Dado o índice n=%d, o seu valor de fibonacci pelo laço 'for' é de: %d\n",n, num);
+    printf("Dado o índice n=%d, o seu valor de fibonacci de forma recursiva é de: %d\n\n", n, fib_recursivo(n));
 
     return 0;
 }
